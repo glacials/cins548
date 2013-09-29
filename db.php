2a592bb988e92c872313116114a5f5ac5bdaed35
nomadmtb <kylegluce@gmail.com>
2013-09-29 15:56:36 -0700

    Updated changes with database connection into

--- db.php
+++ db.php
@@ -3,8 +3,11 @@
 /*
  * Establish a database connection. This file's permissions should be as
  * minimal as possible! If anyone gets read access to this file it's all over.
+ *
+ * Create a user 'webapp' that has access to the 548webapp database with
+ * password 'teamalphacins548webapp'. We will need to change this when we deploy!
  */
 
-$db = new mysqli('localhost', 'database_user_here', 'database_password_here', 'database_name_here');
+$db = new mysqli('localhost', 'webapp', 'teamalphacins548webapp', '548webapp');
 
 ?>
