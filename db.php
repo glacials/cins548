dfd02d951dfc88b73c3df213305bb8b08ca234d1
skoh-fley <qhiiyr@gmail.com>
2013-09-29 12:24:40 -0700

    Commented out fake database connection

--- db.php
+++ db.php
@@ -5,6 +5,6 @@
  * minimal as possible! If anyone gets read access to this file it's all over.
  */
 
-$db = new mysqli('localhost', 'database_user_here', 'database_password_here', 'database_name_here');
+// $db = new mysqli('localhost', 'database_user_here', 'database_password_here', 'database_name_here');
 
 ?>
