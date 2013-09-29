23c445618b099cd309cc20aef0d67c3d63a818e0
nomadmtb <kylegluce@gmail.com>
2013-09-29 16:08:16 -0700

    Added some preliminary files for user authentication

---
+++ login.php
@@ -0,0 +1,5 @@
+/*
+ * This page will authenticate users and create session variables, that tells
+ * the webapp that the user is authenticated, and they are allowed to browse to
+ * other pages. Input will be given via a POST from the login.html template.
+ */
