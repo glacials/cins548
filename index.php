7d0aebd6158b4e0c8bd463a6e5c97ce4dfc942ba
skoh-fley <qhiiyr@gmail.com>
2013-10-01 16:31:49 -0700

    Added a check to see if a user is logged in

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ require_once 'autoload.php';
 $db = new Database;
 
 // Testing the login stuff. The second argument replaces '{page_title}' in header.html.
-$page = new Page('login.html', array('page_title' => 'login'));
-print $page->html;
+if (isset($_SESSION['user'])) {
+  print 'Logged in! <a href="logout.php">Logout</a>';
+} else {
+  $page = new Page('login.html', array('page_title' => 'login'));
+  print $page->html;
+}
 
 ?>
