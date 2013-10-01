65aadfe4e46be21af2644c2c2f49dc3b227e2f02
nomadmtb <kylegluce@gmail.com>
2013-10-01 16:19:27 -0700

    Created the User constructor

--- classes/User.php
+++ classes/User.php
@@ -1,6 +1,27 @@
 <?php
 
 class User {
+
+	private $id;
+	private $username;
+	private $password_hash;
+	private $is_admin;
+	private $gender;
+
+	function __construct($Id, $Username, $Password_hash, $Is_admin, $Gender) {
+		$this->id = $ID;
+		$this->username = $Username;
+		$this->password_hash = $Password_hash;
+		$this->is_admin = $Is_admin;
+		$this->gender = $Gender;
+	}
+
+	public function is_admin() {
+		if ($is_admin)
+			return true;
+		else
+			return false;
+	}
 }
 
 ?>
