ed2e8fa493f57010ef78a8773f96d287ed3a7093
nomadmtb <kylegluce@gmail.com>
2013-10-01 21:52:45 -0700

    Updated Product class

--- classes/Product.php
+++ classes/Product.php
@@ -2,6 +2,20 @@
 
 class Product {
 
+	private $id;
+	private $name;
+	private $image_url;
+	private $description;
+	private $price;
+
+	public function __construct($Id, $Name, $Image_url, $Description, $Price) {
+		$this->id = $Id;
+		$this->name = $Name;
+		$this->image_url = $image_url;
+		$this->description = $Description;
+		$this->price = $Price;
+	}
+
 }
 
 ?>
