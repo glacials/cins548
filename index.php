720235e2d39a3ff536402cb189bfd0c43039cccf
skoh-fley <qhiiyr@gmail.com>
2013-10-03 16:20:27 -0700

    Added product.html and browse.html
    browse.html is a normal page, but product.html will be included several times
    into browse.html. See index.php for an example of how this would happen.

--- index.php
+++ index.php
@@ -15,6 +15,20 @@ if (isset($_GET['login'])) {
   print $page->html;
 } elseif (isset($_GET['signup'])) {
   $page = new Page('signup.html', array('page_title' => 'Sign up'));
+} elseif (isset($_GET['browse'])) {
+  /*
+   * Run each product through its Page, concatenate all that outputted HTML
+   * together, then pass that to the browse.html Page via the product_list
+   * variable.
+   */
+  /* todo: Doesn't yet work
+  $product_list = '';
+  foreach ($db->get_all_products()) {
+    $product_page = new Page('product.html', array('product_name' => $some-name, 'product_desc' => $some-desc, ..etc));
+    $product_list .= $product_page->html
+  }
+   */
+  $page = new Page('browse.html', array('page_title' => 'Browse items', 'product_list' => $product_list));
 } else {
   $page = new Page('index.html', array('page_title' => 'Home'));
 }
