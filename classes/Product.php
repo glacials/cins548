a2661d45a4bce5c88564d2510ed3d8801ed96f5f
nomadmtb <kylegluce@gmail.com>
2013-10-03 11:49:29 -0700

    added Product::get_details()

--- classes/Product.php
+++ classes/Product.php
@@ -15,6 +15,21 @@ class Product {
 		$this->description = $Description;
 		$this->price = $Price;
 	}
+
+	/* get_details()
+	 * Will return an array of data that pertains to the Product. This can be used when
+	 * we need to display products on a webpage. We can use this when passing parameters
+	 * to the template.
+	 */
+	public function get_details() {
+		return array(
+			'product_id' => $this->id,
+			'product name' => $this->name,
+			'product_image_url' => $this->image_url,
+			'product_description' => $this->description,
+			'product_price' => $this->price
+		);
+	}
 }
 
 ?>
