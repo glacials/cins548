781efeb47cc950fa009615c3311fc9e7f7e6c236
gscory <gscory@mail.csuchico.edu>
2013-10-04 13:51:10 -0700

    Update validate.php
    Reference:  http://www.w3schools.com/php/php_form_url_email.asp

--- validate.php
+++ validate.php
@@ -1,19 +1,40 @@
 <?php
 // define variables and set to empty values
-$email = $password = $gender = "";
+$emailErr = $genderErr = $passwordErr = "";
+$email = $gender = $website = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-  $email = test_input($_POST["email"]);
-  $password = test_input($_POST["passworde"]);
-  $gender = test_input($_POST["gender"]);
-}
+  if (empty($_POST["email"]))
+    {$emailErr = "Email is required";}
+  else
+    {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address syntax is valid
+    if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
+      {
+      $emailErr = "Invalid email format";
+      }
+    }
 
-function test_input($data)
-{
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+  if (empty($_POST["password"]))
+    {$password = "";}
+  else
+    {
+// commenting out the following for future development-----------   
+<?php /*  need to modify string to enforce password requirements:   
+    $password = test_input($_POST["password"]);
+   // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$password))
+      {
+      $passwordErr = "Password does not meet requirements";
+      }
+    }
+*/?>
+
+  if (empty($_POST["gender"]))
+    {$genderErr = "Gender is required";}
+  else
+    {$gender = test_input($_POST["gender"]);}
 }
 ?>
