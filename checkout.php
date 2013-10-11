3c9c7b674778a1fa95a052485c1a2e9d9fa6e5fe
nomadmtb <kylegluce@gmail.com>
2013-10-11 13:32:40 -0700

    Checkout correctly displays error if not logged in

--- checkout.php
+++ checkout.php
@@ -1,8 +1,10 @@
 <?php
 
+session_start();
+
 if (!isset($_SESSION['user'])) {
 	$_SESSION['error'] = 'You must be logged in to checkout. Please login.';
-	header('Location: ?login');
+	header('Location: /?login');
 }
 
 ?>
