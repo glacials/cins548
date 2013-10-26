0c06462a02116a7f79541221a8b85c107eed0bc9
nomadmtb <kylegluce@gmail.com>
2013-10-26 15:12:28 -0700

    Added length-checking on the signup-page

--- signup.php
+++ signup.php
@@ -11,6 +11,8 @@ $question = $_POST['question'];
 $answer = hash("sha256", $_POST['answer'] . 'alPha548*3jasc');
 $address = $_POST['address'];
 
+
+
 // Redirect back to signup page if any of the fields are empty, or if the passwords don't match each other.
 if (empty($username)         || empty($password) ||
     empty($password_confirm) || empty($gender)   ||
@@ -19,16 +21,26 @@ if (empty($username)         || empty($password) ||
    ) {
   $_SESSION['error'] = 'All fields must be correctly filled out.';
   header('Location: ?signup');
-} else {
-  // Create new User object with variables, initialize data.
-  $user = new User(0, $username, $password, 0, $gender, 0, $question, $answer, $address);
-  if ($user->save('insert')) {
-    // We are making them login so their user object will be updated (user_id, updated, etc.)
-    $_SESSION['notice'] = 'Account created! Please login.';
-    header('Location: ?login');
-  } else {
-    $_SESSION['error'] = 'There was a problem making your account. Please try again later.';
-    header('Location: ?signup');
-  }
-}
+   } else {
+	   if ((strlen($username) > 30) ||
+	       (strlen($password) > 50) ||
+	       (strlen($gender) > 1)    ||
+	       (strlen($question) > 100)||
+	       (strlen($answer) > 50)   ||
+	       (strlen($address) > 100)) {
+		       $_SESSION['error'] = 'One or more input fields are too long.';
+		       header('Location: ?signup');
+	       } else {
+  			// Create new User object with variables, initialize data.
+  			$user = new User(0, $username, $password, 0, $gender, 0, $question, $answer, $address);
+  			if ($user->save('insert')) {
+    				// We are making them login so their user object will be updated (user_id, updated, etc.)
+    				$_SESSION['notice'] = 'Account created! Please login.';
+    				header('Location: ?login');
+  			} else {
+    				$_SESSION['error'] = 'There was a problem making your account. Please try again later.';
+    				header('Location: ?signup');
+  			}
+	       	}
+   }
 ?>
